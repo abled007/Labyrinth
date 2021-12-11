55dc8d4b1936f93d60843c6b3a2d23056b29a32a
abled007 <audreybledsoe3@gmail.com>
2021-12-11 08:30:11 -0800

    Cover Page Layout

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* cover screen
+    -background hyrule
+    -title  
+    -start button
+        -directs to first maze when clicked
+    -instruction 
+        -pops up instrutions when clicked
+*/
+
+
