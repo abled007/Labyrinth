ced0681d99406ad786e5963ddcba5e458d683c3d
abled007 <audreybledsoe3@gmail.com>
2021-12-11 10:07:44 -0800

    Mazes, Loser, Winner page

--- js/script.js
+++ js/script.js
@@ -7,4 +7,28 @@
         -pops up instrutions when clicked
 */
 
+/* Mazes 
+    -finish first maze to continue to next
+        -timer restarts after every maze 
+    -low visablity
+    -timed each maze
+    -"mystery boxes"
+        -random (lose one life or gain ability);
+        -"doomsday" box
+            -loses game
+            -takes to loser page
+    -cannot accesss same mystery box once used 
+*/
 
+/* Loser page
+    -"game over" black bacground
+    -continue
+        -takes you back to first maze
+        -make sure mazes are reset to default
+*/
+
+/* Winner page 
+    - background 
+    - play again
+        -takes you back to first maze
+*/
