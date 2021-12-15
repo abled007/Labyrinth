702353cc731817a21472cf07c5c5d0b251604a63
abled007 <audreybledsoe3@gmail.com>
2021-12-15 08:33:51 -0800

    Intro hover

--- js/script.js
+++ js/script.js
@@ -10,10 +10,24 @@ $('.container1').append($coverImage);
 const $gameTitle = $('<div class = "title">Labyrinth</div>');
 $('.container1').append($gameTitle);
 
+//How to Play 
+const $gameIntro = $('<div class = "intro">How to Play</div>');
+$('.container1').append($gameIntro);
+
+$(document).ready(
+    $('.intro').hover(
+        function()  {
+            $(this).append($('<h1>How to play</h1>'));
+            $(this).append($('<p>Instructions on how to play</p>')) 
+        }, function() {
+            $(this).find('h1').remove();
+            $(this).find('p').remove();
+        }
+)
+)
+
 // Start button
 const $gameStart = $('<div class = "start">Start</div>');
 $('.container1').append($gameStart);
 
-//How to Play 
-const $gameIntro = $('<div class = "intro">How to Play</div>');
-$('.container1').append($gameIntro);
+
