21d77936d26c3fd2611d9375c711b1b18935e1b1
abled007 <audreybledsoe3@gmail.com>
2021-12-17 20:26:03 -0800

    Tried making Shrine

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // Find Element
 let $canvas = $('#myCanvas');
+let $gameActive = true;
 
 // player 
 let $player = {
@@ -30,98 +31,86 @@ let $gameBoard = [
     [1,1,1,1,1,0,0,0,1,1,0,0,0,1,0,0,0,1,1,1,1],
             
 ];
-               
-// Creating the board
-function drawBoard() {
-    const ctx = $canvas[0].getContext('2d');
-    const blockSize = $gameBoard.length;
-    //player 
-    function $circle() {
+
+//canvas
+const ctx = $canvas[0].getContext('2d');
+const blockSize = $gameBoard.length;
+
+//player
+function $circle() {
     ctx.beginPath();
     let plyr = blockSize/2;
     ctx.fillStyle = '#00aaff';
     ctx.arc($player.x*blockSize+plyr, $player.y*blockSize+plyr, plyr, 0, 2*Math.PI);
     ctx.fill();
-    ctx.translate(11,8);
-    }
-    $circle();
+    //ctx.translate(x,y); //How do I chnage the players positon
+};
 
-
-    //color
+// Creating the board
+function $drawBoard() {
     ctx.fillStyle = "#6E5519";
-    //Positon of board
     ctx.setTransform(1, 0, 0, 1, 0, 0);
-
+    
     function $trapDoor() {
-    for (let y = 0; y < $gameBoard.length; y++) {
-        for (let x = 0; x < $gameBoard[y].length; x++) {
-            if($gameBoard[y][x] === 1) {
-                ctx.fillRect(x*blockSize, y*blockSize, blockSize, blockSize);
+        for (let y = 0; y < $gameBoard.length; y++) {
+            for (let x = 0; x < $gameBoard[y].length; x++) {
+                if($gameBoard[y][x] === 1) {
+                    ctx.fillRect(x*blockSize, y*blockSize, blockSize, blockSize);
                 } else if($gameBoard[y][x] === 3){
-                    // Creating the goal 
                     ctx.beginPath();
                     ctx.lineWidth = 5;
-                    ctx.strokeStyle = "green";
-                    // Creating that X shape
+                    ctx.strokeStyle = "red";
                     ctx.moveTo(x*blockSize, y*blockSize);
                     ctx.lineTo((x+1)*blockSize, (y+1)*blockSize);
                     ctx.moveTo(x*blockSize, (y+1)*blockSize);
                     ctx.lineTo((x+1)*blockSize, y*blockSize);
-                    ctx.stroke();
-
-                }
+                    ctx.stroke(); 
+                } 
             }
         }
     }
-    $trapDoor();
-
-       // Triangle shape player (made the shape but did not move correctly
-       // ctx.beginPath();
-       // ctx.lineWidth = 4;
-       // ctx.moveTo($player.x*blockSize, $player.y*blockSize);
-       // ctx.lineTo(($player.x+1)*blockSize, ($player.y+1)*blockSize);
-       // ctx.lineTo(($player.x)*blockSize, $player.y+1*blockSize);
-       // ctx.closePath();
-       // ctx.strokeStyle = "blue";
-       // ctx.stroke();
-       //ctx.fill();
-       
-    // Player 
-
-    
+    $trapDoor(); 
+    $circle(); //player
+    //$shrine();  
 }
-drawBoard();
+$drawBoard();
 
+//Shrine
+// function $shrine(){
+//     for (let y = 0; y < $gameBoard.length; y++) {
+//         for (let x = 0; x < $gameBoard[y].length; x++) {
+//             if ($gameBoard[y][x] === 1) {
+//                 ctx.fillRect(x*blockSize, y*blockSize, blockSize, blockSize);
+//             }else if($gameBoard[y][x] === 2){
+//                 ctx.beginPath();
+//                 ctx.lineWidth = 5;
+//                 ctx.moveTo($player.x*blockSize, $player.y*blockSize);
+//                 ctx.lineTo(($player.x+1)*blockSize, ($player.y+1)*blockSize);
+//                 ctx.lineTo(($player.x)*blockSize, $player.y+1*blockSize);
+//                 ctx.closePath();
+//                 ctx.strokeStyle = "blue";
+//                 ctx.stroke();
+//                 ctx.fill();
+//                 }
+//             }
+//         }
+//     }
        
 //Making sure player moves 0 board and not on 1
 function canMove(x, y) {
     return (y >= 0) && (y < $gameBoard.length) && (x >= 0) && (x < $gameBoard[y].length) && ($gameBoard[y][x] != 1);
-}
-        
-// $(document).keyup(function(e){
-//     if((e.which == 38) && canMove($player.x, $player.y-1))
-//     $player.y--;
-//     else if((e.which == 40) && canMove($player.x, $player.y+1)) 
-//     $player.y++;
-//     else if((e.which == 37) && canMove($player.x-1, $player.y))
-//     $player.x--;
-//     else if((e.which == 39) && canMove($player.x+1, $player.y))
-//     $player.x++;           
-//     drawBoard();
-//     e.preventDefault();
-//     // 
-// }); 
+};
 
 // arrow keys 
 $(document).keydown(function(e){
     e.preventDefault();
     let key = e.which;
     if((key == 37) && canMove($player.x-1, $player.y)) $player.x--; //left
-    else if((key == 38 && canMove($player.x, $player.y+1))) $player.y++; //up
-    else if((key == 39 && canMove($player.x+1, $player.y))) $player.x--; //right
-    else if((key == 40 && canMove($player.x, $player.y-1))) $player.y++; //down
-    drawBoard();
-})
+    else if((key == 38 && canMove($player.x, $player.y-1))) $player.y--; //up
+    else if((key == 39 && canMove($player.x+1, $player.y))) $player.x++; //right
+    else if((key == 40 && canMove($player.x, $player.y+1))) $player.y++; //down
+    $drawBoard();
+});
 
 
       
