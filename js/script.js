d7a6970c2556d96a914d2c1d0c78a599a6af2aeb
abled007 <audreybledsoe3@gmail.com>
2022-07-04 09:09:42 -0700

    cleaned code

--- js/script.js
+++ js/script.js
@@ -1,57 +1,69 @@
-// Find Element
 let $canvas = $('#myCanvas');
 let $gameActive = true;
 
-// player 
 let $player = {
-    //axis
-    x: 10,
-    y: 15,
+    x: 13,
+    y: 25,
 }
 
-
-// 0 = path, 1 = blocked, 3 = mystery box
 let $gameBoard = [
     
-    [0,1,0,3,0,1,1,0,1,1,1,1,1,0,0,0,0,1,0,0,0],          
-    [0,1,0,1,0,0,1,0,1,0,0,0,1,1,1,1,3,1,1,0,1],
-    [3,1,0,1,1,0,1,0,1,0,2,0,1,0,0,0,0,0,1,0,3],
-    [0,1,0,1,0,0,0,0,1,0,0,0,0,0,1,0,1,0,0,0,1],
-    [0,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1],
-    [0,0,0,0,0,1,0,0,0,0,0,0,0,3,0,1,0,0,0,0,0],
-    [1,1,1,1,0,1,0,0,1,0,0,0,1,0,0,1,0,1,0,1,1],
-    [0,1,0,1,0,0,0,0,3,0,3,0,0,0,0,1,0,1,0,0,0],
-    [0,1,0,1,1,1,0,0,0,0,0,0,0,0,0,1,0,3,1,1,0],
-    [0,3,0,0,0,1,0,0,1,0,0,0,1,0,0,1,0,1,0,0,3],
-    [1,1,1,1,0,1,0,3,0,0,3,0,0,0,0,1,0,1,1,1,1],
-    [0,0,0,1,0,1,0,0,0,0,0,0,0,3,0,1,0,0,0,0,1],
-    [1,1,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1,0,0,0],
-    [0,0,0,1,1,1,0,0,0,0,0,0,0,3,0,1,0,1,0,1,1],
-    [0,1,0,0,0,1,0,0,0,3,2,0,0,0,0,1,0,1,0,0,0],
-    [0,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,1,0,1],
-    [0,1,3,0,0,0,1,0,0,0,0,1,0,0,0,1,0,3,0,0,1],
-    [1,1,1,1,1,0,0,0,1,1,0,0,0,1,0,0,0,1,1,1,1],
+    [0,0,0,0,1,0,3,0,1,1,0,1,1,1,1,1,0,0,0,0,1,0,0,0,0,0,1],          
+    [0,0,0,0,1,0,1,0,0,1,0,1,0,0,0,1,1,1,1,3,1,1,0,1,0,0,0],
+    [1,1,1,3,1,0,1,1,0,1,0,1,0,2,0,1,0,0,0,0,0,1,0,3,0,0,1],
+    [1,1,1,0,1,0,1,0,0,0,0,1,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0],
+    [0,0,0,0,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0,0,0],
+    [0,0,0,0,1,3,0,0,1,0,0,0,0,0,0,0,0,0,1,0,3,0,0,1,0,0,1],
+    [1,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,0,0,0],
+    [1,1,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,0,0,0],
+    [1,1,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,0,0,0],
+    [1,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,0,0,0],
+    [1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,0,0,0],
+    [1,0,1,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,0,0,0],
+    [1,1,1,1,0,1,1,1,1,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,0,0,1],
+    [1,1,1,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,0,0,0],
+    [1,1,1,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,0,0,0],
+    [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,3,0,1,0,0,0,0,0,0,0,0],
+    [0,0,0,1,1,1,1,0,1,0,0,0,0,0,0,0,0,0,1,0,1,0,1,1,0,0,1],
+    [0,0,0,0,1,0,1,0,1,0,0,3,0,3,0,0,0,0,1,0,1,0,0,0,0,0,0],
+    [0,0,0,0,1,0,1,1,1,0,0,0,0,0,0,0,0,0,1,0,3,1,1,0,0,0,0],
+    [0,0,0,0,3,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,1,0,0,3,0,0,1],
+    [1,1,1,1,1,1,1,0,1,0,3,0,0,3,0,0,0,0,1,0,1,1,1,1,0,0,0],
+    [1,1,1,0,0,0,1,0,1,0,0,0,0,0,0,0,3,0,1,0,0,0,0,1,0,0,1],
+    [0,0,0,1,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0],
+    [1,1,1,0,0,0,1,1,1,0,0,0,0,0,0,0,3,0,1,0,1,0,1,1,0,0,1],
+    [1,1,1,0,1,0,0,0,1,0,0,0,3,2,0,0,0,0,1,0,1,0,0,0,0,0,0],
+    [0,0,0,0,1,1,1,0,1,1,1,1,0,0,0,1,1,1,1,1,1,1,0,1,0,0,1],
             
 ];
 
-//canvas
+let $goal = $gameBoard[2]
+
+let $trap = $gameBoard[3]
+
 const ctx = $canvas[0].getContext('2d');
 let blockSize = $gameBoard.length;
-
-//player
+let width = $canvas.width();
 
 $().toggleClass
-ctx.clearRect(0, 0, $canvas.width, $canvas.height);
 
-// Creating the board
+function $user() { 
+    ctx.beginPath();
+    ctx.clearRect(0, 0, $canvas.width, $canvas.width);
+    let half = blockSize/2;
+    ctx.fillStyle = '#00aaff';
+    ctx.arc($player.x*blockSize+half, $player.y*blockSize+half, half, 0, 2*Math.PI);
+    ctx.fill();
+};
+
 function $drawBoard() {
-    ctx.clearRect(0, 0, $canvas.width, $canvas.height);
+    ctx.clearRect(0, 0, $canvas.width, $canvas.width);
     ctx.fillStyle = "#6E5519";
     ctx.setTransform(1, 0, 0, 1, 0, 0);
     for (let y = 0; y < $gameBoard.length; y++) {
         for (let x = 0; x < $gameBoard[y].length; x++) {
             if($gameBoard[y][x] === 1) {
-                ctx.clearRect(0, 0, $canvas.width, $canvas.height);
+                ctx.clearRect(0, 0, $canvas.width, $canvas.width);
                 ctx.fillRect(x*blockSize, y*blockSize, blockSize, blockSize);
             } else if($gameBoard[y][x] === 2){
                 ctx.beginPath();
@@ -74,22 +86,13 @@ function $drawBoard() {
             }
         }
     }
-    function $circle() { //player
-        ctx.beginPath();
-        ctx.clearRect(0, 0, $canvas.width, $canvas.height);
-        let half = blockSize/2;
-        ctx.fillStyle = '#00aaff';
-        ctx.arc($player.x*blockSize+half, $player.y*blockSize+half, half, 0, 2*Math.PI);
-        ctx.fill();
-    };
-    $circle();
+    $user();
 }
 $drawBoard();
 
 const $playerStatus = $('<h1 class="status"></h1>');
 $('body').append($playerStatus);
 
-//Winner
 function $checkWin() {
     for (let i = 0; i < $gameBoard.length; i++){
         if($gameBoard[$player.x][$player.y] === 2){
@@ -102,7 +105,6 @@ function $checkWin() {
 };
 $checkWin();
 
-//Create lives 
 let $fullLives = [0,0,0];
 let $twoLives = [0,0,1];
 let $oneLife = [0,1,1];
@@ -112,44 +114,26 @@ let $heart = $('.bi bi-suit-heart-fill');
 let $noHeart = $('bi bi-suit-heart');
 
   
-    const $livesGrid = $('<div class="grid"></div>');
-    $('body').append($livesGrid);
+const $livesGrid = $('<div class="grid"></div>');
+$('body').append($livesGrid);
     
-    let $livesLeft = ["", "", ""];
-    $.each($livesLeft, function(index, value){
-        $('.grid').append(value);
-    });
+let $livesLeft = ["", "", ""];
+$.each($livesLeft, function(index, value) {
+    $('.grid').append(value);
+});
     
-    
-    //Create a function when player lands on trapDoor they loose a life
-    
-    
-    
-//Making sure player moves 0 board and not on 1
 function canMove(x, y) {
     return (y >= 0) && (y < $gameBoard.length) && (x >= 0) && (x < $gameBoard[y].length) && ($gameBoard[y][x] != 1);
 };
 
-
-// arrow keys 
 $(document).keydown(function(e){
     e.preventDefault();
     console.log(e);
     let key = e.which;
     if((key == 37) && canMove($player.x-1, $player.y)) $player.x--;
-    //left
     else if((key == 38 && canMove($player.x, $player.y-1))) $player.y--;
-    //up
     else if((key == 39 && canMove($player.x+1, $player.y))) $player.x++;
-    //right
     else if((key == 40 && canMove($player.x, $player.y+1))) $player.y++;
-    //down
     $drawBoard();
     $checkWin();
 });
-
-// $(document).ready(function(){
-//     ctx.clearRect(0, 0, $canvas.width, $canvas.height);
-//     $('body').toggleClass('#myCanvas', false);  
-// });
-
